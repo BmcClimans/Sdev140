6438a06d85ce71a8331ee500ac86ad9f7870a1bd
BmcClimans <breemcclimans@gmail.com>
2026-09-04 19:20:47 -0400

    Update McClimansBreeannaM02_Ch4Ex13.py

--- Homework/McClimansBreeannaM02_Ch4Ex13.py
+++ Homework/McClimansBreeannaM02_Ch4Ex13.py
@@ -7,10 +7,10 @@ Write a program that uses nested loops to draw this pattern:
 #    #
 #     #
 """
-
+#r for row and c for column
 for r in range(6):
-    for c in range(r+1):
-        if c == 0 or c == r:
+    for c in range(r+2):
+        if c == 0 or c == r + 1:
             print("#", end="")
         else:
             print(" ", end="")
