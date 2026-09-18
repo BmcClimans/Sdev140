2fce9414b999ca77680bb669ea5057c3df74bd91
BmcClimans <breemcclimans@gmail.com>
2026-09-18 17:32:53 -0400

    homework

---
+++ Homework/McClimansBreeannaM04_Day9.py
@@ -0,0 +1,5 @@
+"""
+Download both program.  
+The modify the Day 7 program to use the Die() class in the Ch_10_
+Die_ClassDefined.py file.
+"""
---
+++ Homework/McClimansBreeannaM04_Midterm.py
@@ -0,0 +1,26 @@
+"""
+Your assignment is to write a program that inputs a sentence from the keyboard, word by word, into a list.  The program should output the following.
+
+1. The complete sentence, with only the first letter of the first word capitalized (if it wasn't already), spaces between each word, and a period at the end.
+
+2. The count of the number of words in the sentence.
+
+Hint: end input on a blank line or on input of the word "done".
+
+For instance, if the input is: 
+the
+
+cat
+
+ran 
+
+home
+
+quickly
+
+Your program should output:
+
+The cat ran home quickly.
+There are 5 words in the sentence.
+"""
+
